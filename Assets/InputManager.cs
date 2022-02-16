83914cdf274d6a46ca6cc681c51fd939d53f47a1
Theresa Shope <tburns13@butlercc.edu>
2022-02-16 14:12:21 -0600

    Added FPS controller and shooting mechanic

---
+++ Assets/InputManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    public Gun gun; // should this route through the playercontroller? perhaps...
+
+    public bool debug = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mouse = Mouse.current;
+        if(mouse == null) return;
+
+        if(mouse.leftButton.wasPressedThisFrame) {
+            if(debug) Debug.Log("Left Mouse Button was pressed this frame.");
+
+            if(gun != null) {
+                gun.Fire(); // calling the fire method of the gun variable.
+            } else {
+                if(debug) Debug.Log("There is no gun to fire!");
+            }
+        }
+    }
+}
